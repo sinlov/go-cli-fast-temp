46590e459873c20f1207b85c81e5898ffcb5d53a
sinlov <sinlovgmppt@gmail.com>
2018-02-28 02:33:49 +0800

    let update by dep

---
+++ vendor/github.com/sinlov/golang_utils/cli/cliColor.go
@@ -0,0 +1,116 @@
+package cli
+
+import (
+	"runtime"
+	"fmt"
+)
+
+const (
+	TextBlack   = iota + 30
+	TextRed
+	TextGreen
+	TextYellow
+	TextBlue
+	TextMagenta
+	TextCyan
+	TextWhite
+)
+
+func IsWindows() bool {
+	if runtime.GOOS == "windows" {
+		return true
+	} else {
+		return false
+	}
+}
+
+func textColor(color int, str string) string {
+	if IsWindows() {
+		return str
+	}
+
+	switch color {
+	case TextBlack:
+		return fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", TextBlack, str)
+	case TextRed:
+		return fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", TextRed, str)
+	case TextGreen:
+		return fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", TextGreen, str)
+	case TextYellow:
+		return fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", TextYellow, str)
+	case TextBlue:
+		return fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", TextBlue, str)
+	case TextMagenta:
+		return fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", TextMagenta, str)
+	case TextCyan:
+		return fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", TextCyan, str)
+	case TextWhite:
+		return fmt.Sprintf("\x1b[0;%dm%s\x1b[0m", TextWhite, str)
+	default:
+		return str
+	}
+}
+
+func Black(str string) string {
+	return textColor(TextBlack, str)
+}
+
+func FmtBlack(str string, a ... interface{}) {
+	fmt.Print(Black(fmt.Sprintf(str, a...)))
+}
+
+func Red(str string) string {
+	return textColor(TextRed, str)
+}
+
+func FmtRed(str string, a ... interface{}) {
+	fmt.Print(Red(fmt.Sprintf(str, a...)))
+}
+
+func Green(str string) string {
+	return textColor(TextGreen, str)
+}
+
+func FmtGreen(str string, a ... interface{}) {
+	fmt.Print(Green(fmt.Sprintf(str, a...)))
+}
+
+func Yellow(str string) string {
+	return textColor(TextYellow, str)
+}
+
+func FmtYellow(str string, a ... interface{}) {
+	fmt.Print(Yellow(fmt.Sprintf(str, a...)))
+}
+
+func Blue(str string) string {
+	return textColor(TextBlue, str)
+}
+
+func FmtBlue(str string, a ... interface{}) {
+	fmt.Print(Blue(fmt.Sprintf(str, a...)))
+}
+
+func Magenta(str string) string {
+	return textColor(TextMagenta, str)
+}
+
+func FmtMagenta(str string, a ... interface{}) {
+	fmt.Print(Magenta(fmt.Sprintf(str, a...)))
+}
+
+func Cyan(str string) string {
+	return textColor(TextCyan, str)
+}
+
+func FmtCyan(str string, a ... interface{}) {
+	fmt.Print(Cyan(fmt.Sprintf(str, a...)))
+}
+
+func White(str string) string {
+	return textColor(TextWhite, str)
+}
+
+func FmtWhite(str string, a ... interface{}) {
+	fmt.Print(White(fmt.Sprintf(str, a...)))
+}
---
+++ vendor/github.com/sinlov/golang_utils/cli/cmd.go
@@ -0,0 +1,198 @@
+package cli
+
+import (
+	"os/exec"
+	"strings"
+	"os"
+	"runtime"
+	"github.com/axgle/mahonia"
+	"io/ioutil"
+	"fmt"
+	"github.com/pkg/errors"
+)
+
+var exitWithOutZero = errors.New("Has not run command exit code with 0")
+
+type CmdTea struct {
+	ChartSet   string
+	IsPrint    bool
+	CmdStrings string
+
+	ErrorInfo error
+	IsSuccess bool
+	Pid       int
+	ExitState string
+	Out       string
+	Err       string
+
+	Env       []string
+	ShellPath string
+	Dir       string
+	Args      []string
+}
+
+func getSystem() string {
+	return runtime.GOOS
+}
+
+func IsSysWindows() bool {
+	return getSystem() == "windows"
+}
+
+// chartSet "" default in windows "gbk", other is "utf-8"
+// isPrint default is false
+// return isSuccess bool PID processState string and cmdOut string
+func (ct *CmdTea) CmdTeaInit(chartSet string, isPrint bool, cmd ...string) {
+	var cmdStr []string
+	for _, ct := range cmd {
+		trim := strings.Trim(ct, " ")
+		cmdStr = append(cmdStr, trim)
+	}
+	cmdString := strings.Join(cmdStr, " ")
+	ct.ChartSet = chartSet
+	ct.IsPrint = isPrint
+	ct.CmdStrings = cmdString
+}
+
+func (ct CmdTea) CmdTeaRun() (bool, CmdTea) {
+	if ct.CmdStrings == "" {
+		ct.ErrorInfo = errors.New("You Cmd is Empty!")
+		return false, ct
+
+	}
+	var c *exec.Cmd
+	if IsSysWindows() {
+		argArray := strings.Split("/c "+ct.CmdStrings, " ")
+		c = exec.Command("cmd", argArray...)
+	} else {
+		c = exec.Command("/bin/sh", "-c", ct.CmdStrings)
+	}
+	out, combinedErr := c.CombinedOutput()
+
+	ct.Env = c.Env
+	ct.ShellPath = c.Path
+	ct.Dir = c.Dir
+	ct.Args = c.Args
+
+	var processState os.ProcessState
+	c.ProcessState = &processState
+	processPid := processState.Pid()
+	ct.Pid = processPid
+	var dec mahonia.Decoder
+	if ct.ChartSet == "" {
+		if IsSysWindows() {
+			ct.ChartSet = "gbk"
+		} else {
+			ct.ChartSet = "utf-8"
+		}
+	}
+	dec = mahonia.NewDecoder(ct.ChartSet)
+
+	cmdInterfaceOut := dec.ConvertString(string(out))
+	if ct.IsPrint {
+		fmt.Println(cmdInterfaceOut)
+
+	}
+	if combinedErr != nil {
+		ct.IsSuccess = false
+		ct.Err = cmdInterfaceOut
+		ct.ErrorInfo = exitWithOutZero
+		ct.ExitState = combinedErr.Error()
+		return false, ct
+	} else {
+		ct.IsSuccess = true
+		ct.Out = cmdInterfaceOut
+		ct.ExitState = processState.String()
+		return true, ct
+	}
+}
+
+// chartSet "" default in windows "gbk", other is "utf-8"
+func CmdExec(chartSet string, cmd ...string) (bool, int, string, string) {
+	var c *exec.Cmd
+	var cmdStr []string
+	for _, ct := range cmd {
+		trim := strings.Trim(ct, " ")
+		cmdStr = append(cmdStr, trim)
+	}
+	cmdString := strings.Join(cmdStr, " ")
+
+	if IsSysWindows() {
+		argArray := strings.Split("/c "+cmdString, " ")
+		c = exec.Command("cmd", argArray...)
+	} else {
+		c = exec.Command("/bin/sh", "-c", cmdString)
+	}
+
+	var cmdOut string
+	//c.Stderr = os.Stderr do not set
+	out, err := c.CombinedOutput()
+	var processState os.ProcessState
+	c.ProcessState = &processState
+	processSuccess := processState.Success()
+	processPid := processState.Pid()
+	processStateStr := processState.String()
+	cmdOut = string(out)
+	if IsSysWindows() {
+		if chartSet == "" {
+			chartSet = "gbk"
+		}
+	} else {
+		if chartSet == "" {
+			chartSet = "utf-8"
+		}
+	}
+
+	dec := mahonia.NewDecoder(chartSet)
+	cmdOut = dec.ConvertString(cmdOut)
+	if err != nil {
+		return false, processPid, err.Error(), cmdOut
+	}
+	return processSuccess, processPid, processStateStr, cmdOut
+}
+
+// chartSet "" default in windows "gbk", other is "utf-8"
+func CmdRun(chartSet string, cmd ...string) (bool, error) {
+	var c *exec.Cmd
+	var cmdStr []string
+	for _, ct := range cmd {
+		trim := strings.Trim(ct, " ")
+		cmdStr = append(cmdStr, trim)
+	}
+	cmdString := strings.Join(cmdStr, " ")
+
+	if IsSysWindows() {
+		argArray := strings.Split("/c "+cmdString, " ")
+		c = exec.Command("cmd", argArray...)
+	} else {
+		c = exec.Command("/bin/sh", "-c", cmdString)
+	}
+	if chartSet == "" {
+		if IsSysWindows() {
+			chartSet = "gbk"
+		} else {
+			chartSet = "utf-8"
+		}
+	}
+	dec := mahonia.NewDecoder(chartSet)
+	stdout, err := c.StdoutPipe()       //print to cmd stdout
+	stdErr, stderrErr := c.StderrPipe() //print to cmd stdErr
+	c.Start()
+	content, err := ioutil.ReadAll(stdout)
+	contentErr, stderrErr := ioutil.ReadAll(stdErr)
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+	if stderrErr != nil {
+		fmt.Println(stderrErr)
+		return false, stderrErr
+	}
+	if len(contentErr) > 0 {
+		fmt.Println(Red(dec.ConvertString(string(contentErr))))
+		return false, exitWithOutZero
+	} else {
+		fmt.Println(dec.ConvertString(string(content)))
+		return true, nil
+	}
+}
